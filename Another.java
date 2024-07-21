58ea927599580ab49d83205947ca9516ddb3ba6d
Vedant-07 <vedantnpatel7@gmail.com>
2024-07-21 12:27:35 +0530

    changes from local to crete conflict

--- Another.java
+++ Another.java
@@ -2,7 +2,7 @@ public class Another {
     public static void main(String[] args) {
         System.out.println("Another one ");
     }
-    //adding changes from origin
+    //adding changes from local branch
     //adding this comment to delete it later
     //edit from origin
 }
